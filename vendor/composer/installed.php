55694e64293d187043dca4a5fe15ed9c7fba70e3
Piotr Markiewicz <piotr.markiewicz@ttms.pl>
2022-10-15 00:42:06 +0200

    init

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'processwire/processwire',
+        'pretty_version' => '3.0.200',
+        'version' => '3.0.200.0',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'processwire/processwire' => array(
+            'pretty_version' => '3.0.200',
+            'version' => '3.0.200.0',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
